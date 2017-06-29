5215af71162b7bad20adc64596ec40cc22ced1ad
Alexander Raschid <alexraschid1337@gmail.com>
2017-06-29 19:36:32 +0000

    Added contact section, Updated Store location,sign up popup, contact email response

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-$(document).ready(function() {
+$('#myModal').on('shown.bs.modal', function() {
+    if ($("#myMail").val().length != 0 &&
+        $("#myName").val().length != 0) {
+        $('#myInput').focus()
+        $(".modal-body").html("<p>Hi thank you " + $("#myName").val() + ", for signing up! We will send an email to " + $("#myMail").val() + " shortly.</p>");
+    }
+    else {
+        $(".modal-body").html(" ");
+    }
+});
 
-   
-    
+
+
+$(document).ready(function() {
+    //HI - <name> thank you for signing up! We will send an email to <email> shortly”
+
+    //For the Sign Up Button  
+
 
 });
